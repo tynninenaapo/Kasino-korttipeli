447a65dc6aeabc97d612e3cea16620e4a87dbfb9
tynnina1 <aapo.tynninen@aalto.fi>
2025-03-05 14:43:02 +0200

    Pöytä-luokan kommentointi

--- Koodit/poyta.py
+++ Koodit/poyta.py
@@ -2,15 +2,19 @@ from kortti import Kortti
 
 class Poyta:
 
+    # Pöytä-olion alustus
     def __init__(self):
-        self.poydan_kortit = []
-        self.pakka = []
+        self.poydan_kortit = []     # Lista pöydässä olevista Kortti-olioista
+        self.pakka = []             # Lista pakassa olevista Kortti-olioista
 
+    # Lisää kortin pöytään
     def lisaa_kortti_poytaan(self, kortti):
         self.poydan_kortit.append(kortti)
 
+    # Poistaa kortin pöydästä
     def poista_kortti_poydasta(self, kortti):
         self.poydan_kortit.remove(kortti)
 
+    # Poistaa kortin pakasta
     def poista_kortti_pakasta(self, kortti):
         self.pakka.remove(kortti)

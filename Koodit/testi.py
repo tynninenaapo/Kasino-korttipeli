a89098b2716dd8fe6622af822d99e37ba52eb20e
tynnina1 <aapo.tynninen@aalto.fi>
2025-03-20 19:11:34 +0200

    Yksikkötestit korttientarkistusmetodille

---
+++ Koodit/testi.py
@@ -0,0 +1,45 @@
+import unittest
+from kortti import Kortti
+from poyta import Poyta
+from peli import Peli
+
+class Testi(unittest.TestCase):
+
+    def testaa_korttien_tarkistusta1(self):
+        poyta = Poyta()
+        peli = Peli(poyta)
+
+        valitut_kortit = [Kortti("Ruutu", 10)]
+        pelattu_kortti = Kortti("Pata", 10)
+        self.assertTrue(peli.tarkista_kortti(pelattu_kortti, valitut_kortit))
+
+        pelattu_kortti = Kortti("Pata", 9)
+        self.assertFalse(peli.tarkista_kortti(pelattu_kortti, valitut_kortit))
+
+    def testaa_korttien_tarkistusta2(self):
+        poyta = Poyta()
+        peli = Peli(poyta)
+
+        valitut_kortit = [Kortti("Pata", 4), Kortti("Ruutu", 7)]
+        pelattu_kortti = Kortti("Hertta", 11)
+        self.assertTrue(peli.tarkista_kortti(pelattu_kortti, valitut_kortit))
+
+    def testaa_korttien_tarkistusta3(self):
+        poyta = Poyta()
+        peli = Peli(poyta)
+
+        valitut_kortit = [Kortti("Pata", 4), Kortti("Ruutu", 7), Kortti("Hertta", 11)]
+        pelattu_kortti = Kortti("Risti", 11)
+        self.assertTrue(peli.tarkista_kortti(pelattu_kortti, valitut_kortit))
+
+    def testaa_korttien_tarkistusta4(self):
+        poyta = Poyta()
+        peli = Peli(poyta)
+
+        valitut_kortit = [Kortti("Pata", 4), Kortti("Ruutu", 7),
+                          Kortti("Hertta", 11), Kortti("Risti", 8), Kortti("Pata", 3)]
+        pelattu_kortti = Kortti("Risti", 11)
+        self.assertTrue(peli.tarkista_kortti(pelattu_kortti, valitut_kortit))
+
+if __name__ == '__main__':
+    unittest.main()

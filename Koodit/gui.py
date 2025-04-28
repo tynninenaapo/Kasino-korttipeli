e50b25f1420800668a255c230b59ba1659eec1d1
tynnina1 <aapo.tynninen@aalto.fi>
2025-04-28 21:09:06 +0300

    Pelaajienlisaysikkunan aloitus

--- Koodit/gui.py
+++ Koodit/gui.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QLineEdit, QVBox
 from PyQt6.QtCore import Qt
 
 
-class GUI(QMainWindow):
+class Aloitusikkuna(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -14,17 +14,44 @@ class GUI(QMainWindow):
 
         self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.aloita_peli_nappi = QPushButton("Aloita peli")
+        self.uusi_peli_nappi = QPushButton("Uusi peli")
         self.jatka_pelia_nappi = QPushButton("Jatka peliä")
-        self.aloita_peli_nappi.setFixedSize(600, 200)
+        self.uusi_peli_nappi.setFixedSize(600, 200)
         self.jatka_pelia_nappi.setFixedSize(600, 200)
 
-        self.layout.addWidget(self.aloita_peli_nappi)
+        self.layout.addWidget(self.uusi_peli_nappi)
         self.layout.addWidget(self.jatka_pelia_nappi)
+
+        self.uusi_peli_nappi.clicked.connect(self.uusi_peli_painettu)
 
         self.setCentralWidget(self.paa_widget)
         self.paa_widget.setLayout(self.layout)
 
+        self.showFullScreen()
+
+    def uusi_peli_painettu(self):
+        self.pelaajienlisaysikkuna = Pelaajienlisaysikkuna()
+        self.pelaajienlisaysikkuna.show()
+
+        self.close()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Escape:
+            self.close()
+
+class Pelaajienlisaysikkuna(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Kasino")
+
+        self.paa_widget = QWidget()
+        self.layout = QVBoxLayout()
+
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(self.paa_widget)
+        self.paa_widget.setLayout(self.layout)
         self.showFullScreen()
 
     def keyPressEvent(self, event):
